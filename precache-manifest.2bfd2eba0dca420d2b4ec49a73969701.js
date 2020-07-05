83dfbf15cd99a1f94cbf60e2449111198c203b3d
Denys <denysbraun1985@gmail.com>
2020-07-05 12:30:03 +0300

    download build files

---
+++ precache-manifest.2bfd2eba0dca420d2b4ec49a73969701.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17731b4004a0b60e276ab35b6a64a4ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8735e64a55a0b931e960",
+    "url": "/static/css/main.6ac00c35.chunk.css"
+  },
+  {
+    "revision": "a65fb649e2d066fe7274",
+    "url": "/static/js/2.4529b77d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4529b77d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8735e64a55a0b931e960",
+    "url": "/static/js/main.8a9d68dd.chunk.js"
+  },
+  {
+    "revision": "e225eee929373d7c8b81",
+    "url": "/static/js/runtime-main.753d5c69.js"
+  },
+  {
+    "revision": "5abdeed0d2d29a2c1dc2fed475fc9735",
+    "url": "/static/media/kitchen_bg.5abdeed0.jpg"
+  }
+]);
